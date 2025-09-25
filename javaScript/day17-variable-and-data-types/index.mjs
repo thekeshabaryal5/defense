270e5b36c6ad9c55b785d2460d4f1f6b697f1491
Keshab Aryal <keshabaryal152@gmail.com>
2025-09-25 21:36:30 +0545

    add day17

---
+++ javaScript/day17-variable-and-data-types/index.mjs
@@ -0,0 +1,44 @@
+// var name = "keshab";
+// if (true) {
+//   console.log(name);
+//   name = "aryal";
+// }
+
+// console.log(name);
+
+// function fun() {
+//   var name = "marija";
+//   console.log(name);
+// }
+
+// fun();
+
+// console.log(name)
+
+let a = 10;
+console.log(a);
+
+let b = 2.36;
+console.log(a, b);
+
+let c = "Strike is comming";
+let d = "Anjali";
+
+let ram = "He said that 'keshab is comming'";
+console.log(ram);
+
+let isTrue = true;
+console.log(isTrue);
+
+let user;
+console.log(user);
+
+// bigInt
+let num = 23343243434342525n;
+console.log(num);
+
+let arr1 = [1, "ram", true, "shyam"];
+console.log(arr1);
+
+console.log(typeof arr1);
+console.log(arr1.name);
