b442fe09d09b8f95d29c85fb0694d6b643d0962e
Keshab Aryal <keshabaryal152@gmail.com>
2025-10-06 12:40:31 +0545

    add day19

---
+++ javaScript/day19-conditions-loops-operators/index.mjs
@@ -0,0 +1,21 @@
+let z = "123";
+let y = 123;
+// console.log(z == y);
+
+// let a = "10";
+// let b = Number(a);
+// console.log(b);
+// console.log(typeof b)
+
+// console.log("Hello");
+
+// 0.1 + 0.2  = 0.3;
+
+let a = 0.1;
+let b = 0.2;
+// console.log(a + b);
+
+console.log(true || false);
+console.log(false || true);
+
+console.log("h" || "Mohit")
