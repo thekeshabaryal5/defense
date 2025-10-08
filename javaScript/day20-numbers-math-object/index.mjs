eea89a19cea3153232ddc28bb191d51a346a59b9
Keshab Aryal <keshabaryal152@gmail.com>
2025-10-08 12:29:54 +0545

    add day20

---
+++ javaScript/day20-numbers-math-object/index.mjs
@@ -0,0 +1,47 @@
+// let a = 10;
+// console.log(a);
+
+// let b = 345.6821;
+// console.log(b.toFixed(2));
+
+// console.log(typeof b.toFixed(1));
+
+// console.log(b.toPrecision(4));
+
+// let myNumber = new Number(10);
+// console.log(typeof myNumber);
+// console.log(myNumber);
+
+// let b = 22.534;
+// console.log(b.toPrecision(1));
+
+// console.log(Math.abs(-452));
+// console.log(Math.PI);
+
+// let num = Math.random() * 20;
+// console.log(num);
+// console.log(num.toFixed(0));
+
+// let randomNumber = Math.random();
+// let randomNumber10 = randomNumber * 10;
+// let randomNumber20 = randomNumber * 20;
+// console.log("Random number generated is: ", randomNumber);
+// console.log("Random number10 generated is: ", randomNumber10);
+// console.log("Random number20 generated is: ", randomNumber20);
+
+let randomNumber = Math.random();
+// console.log(Math.floor(((randomNumber * 10) % 6) + 1));
+
+// for (let i = 0; i < 20; i++) console.log(Math.floor(Math.random() * 11) + 15);
+
+// console.log(Math.floor(Math.random() * (9999 - 1000 + 1) + 1000))
+
+function newFunction() {
+  let a = 0;
+  let b = 0.99999;
+  console.log((b - a) / 10);
+}
+
+// newFunction();
+let cruTime = new Date();
+console.log(cruTime.toLocaleTimeString());
