5456f4baed2099d43fa6efd649b2888c0482a5a4
Keshab Aryal <keshabaryal152@gmail.com>
2025-10-09 19:27:16 +0545

    add day21

---
+++ javaScript/day21-strings-dates/index.mjs
@@ -0,0 +1,28 @@
+// let str1 = "keshab";
+// console.log(str1.length);
+
+// let str1 = "perfection is just an illusion";
+// console.log(str1.lastIndexOf("per"));
+
+// let str1 = "perfection is an illusion";
+// console.log(str1.includes("IS"));
+// console.log(str1.slice(-4,-1));
+
+// console.log(str1.substring(8,1));
+
+// let date = new Date();
+// console.log(date.toLocaleTimeString());
+// console.log(date.getMonth());
+
+// const now = Date.now();
+// // console.log(now);
+// console.log(new Date(0));
+
+// const date = new Date(1749999999999);
+// 1759992765152
+// console.log(date.toString());
+
+// console.log(Date.now());
+
+const date = new Date();
+console.log(date.toString());
